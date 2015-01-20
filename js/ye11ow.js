f67ca47c63438ab37e2141aec70ac49cd978aa0d
Min Zhang <minzhsh@cn.ibm.com>
2015-01-20 22:50:35 +0800

    Added some details.
    Implemented links
    Added font awesome
    Added multi-lang

--- js/ye11ow.js
+++ js/ye11ow.js
@@ -1,29 +1,28 @@
 $(document).ready( function(){
 
   window.sr = new scrollReveal({
-    wait: '0.3s',
-    move: '50px',
-    enter: 'left'
+    wait: "0.3s",
+    move: "50px",
+    enter: "left"
   });
 
   var links = new Walkway({
-    selector: '.links > svg',
+    selector: ".links > svg",
     duration: 1000
   });
 
-  var $timeline_block = $('.cd-timeline-block');
+  var $timeline_block = $(".cd-timeline-block");
 
   $timeline_block.each(function(){
     if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
-      $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+      $(this).find(".cd-timeline-img, .cd-timeline-content").addClass("is-hidden");
     }
   });
 
-  $(window).on('scroll', function(){
+  $(window).on("scroll", function(){
     $timeline_block.each(function(){
-      if( $(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.5 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
-        console.log("show");
-        $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+      if( $(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.5 && $(this).find(".cd-timeline-img").hasClass("is-hidden") ) {
+        $(this).find(".cd-timeline-img, .cd-timeline-content").removeClass("is-hidden").addClass("bounce-in");
       }
     });
 
@@ -38,12 +37,30 @@ $(document).ready( function(){
         });
       });
     }
-
   });
 
+  $(".links").on("click", "svg", function(event) {
+    var $target = $(event.currentTarget),
+        link = $target.attr("data-target");
 
+    window.open(link, "_blank");
+  });
+
+  $(".links").on("mouseover", "svg", function(event) {
+    var $target = $(event.currentTarget);
+
+    $target.find("path").css("stroke", "red");
+    $target.find("text").css("fill", "red");
+  });
+
+  $(".links").on("mouseout", "svg", function(event) {
+    var $target = $(event.currentTarget);
+
+    $target.find("path").css("stroke", "#2780e3");
+    $target.find("text").css("fill", "#2780e3");
+  });
 
 
 });
 
-var name = "章旻";
+var myName = "章旻";

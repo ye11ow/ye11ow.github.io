c52e0b07653767652a1b3362461a74f78966bcf1
ye11ow <ye111111ow@gmail.com>
2015-01-27 23:32:41 +0800

    Removed timeline. Added work exp details. Seperate work exp and open source exp

--- js/ye11ow.js
+++ js/ye11ow.js
@@ -11,21 +11,7 @@ $(document).ready( function(){
     duration: 1000
   });
 
-  var $timeline_block = $(".cd-timeline-block");
-
-  $timeline_block.each(function(){
-    if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
-      $(this).find(".cd-timeline-img, .cd-timeline-content").addClass("is-hidden");
-    }
-  });
-
   $(window).on("scroll", function(){
-    $timeline_block.each(function(){
-      if( $(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.5 && $(this).find(".cd-timeline-img").hasClass("is-hidden") ) {
-        $(this).find(".cd-timeline-img, .cd-timeline-content").removeClass("is-hidden").addClass("bounce-in");
-      }
-    });
-
     if ($(".findme").offset().top <= $(window).scrollTop() + $(window).height() * 0.75) {
       $(".findme").animate({
         opacity: 1
